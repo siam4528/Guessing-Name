5fd2183351ae1e48b48c894abd5e8a077985361c
siam4528 <135732529+siam4528@users.noreply.github.com>
2023-08-28 21:57:33 +0600

    Add files via upload

---
+++ guessing_name.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<string.h>
+//#include<stdlib.h>
+int main()
+{
+    char name[20],gName[20];
+    printf("Enter a Name : ");
+    scanf("%s",&name);
+    system("cls");
+    for(int i=0;i<3;i++)
+    {
+        printf("Enter guessing name : ");
+        scanf("%s",&gName);
+        if(strcmp(gName,name)==0)
+        {
+            printf("You Win.\n");
+            break;
+        }
+        else{
+            printf("You lost.\n");
+        }
+
+    }
+    printf("\n\nGuessed right name : %s",name);
+
+    return 0;
+}
